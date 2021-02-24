81356c452971a8ce043a6e692f7a479e3c5d5cab
Elías Ferreira <eliosferre3@hotmail.com>
2021-02-24 15:09:28 -0300

    Update playing functions and stop the game at score 5

--- javascript.js
+++ javascript.js
@@ -13,9 +13,9 @@ function computerPick(){
 }
 
 //Create a function that plays a single round of the game
-function playRound(playerSelection){
+function playRound(playerSelection) {
     computerSelection = computerPick();
-        if (computerSelection == playerSelection) {
+    if (computerSelection == playerSelection) {
         gameText.textContent = "It's a tie!";
     }
     else if (computerSelection == "rock" && playerSelection == "scissors") {
@@ -39,23 +39,59 @@ function playRound(playerSelection){
         playerScore++;
     }
     else if (computerSelection == "scissors" && playerSelection == "rock") {
-        gameText.textContent ="You Win! Rock beats Scissors!";
+        gameText.textContent = "You Win! Rock beats Scissors!";
         playerScore++;
     }
+    computerSText.textContent = `Computer Selection: ${computerSelection}`;
     results.textContent = `Player Score: ${playerScore} - Computer Score: ${computerScore}`;
+    playGame();
 }
 
-//Button to play a round of the game with rock as playerSelection
+//Creates a function that plays the game until the player or computer reaches 5 points
+function playGame() 
+{
+    if (playerScore == 5) 
+    {
+        gameText.textContent = "Congratulations! You won.";
+        rockButton.removeEventListener("click", playRock);
+        paperButton.removeEventListener("click", playPaper);
+        scissorsButton.removeEventListener("click", playScissors);
+    }
+    else if (computerScore == 5)
+    {
+        gameText.textContent = "You lost :(. Better luck next time!";
+        rockButton.removeEventListener("click", playRock);
+        paperButton.removeEventListener("click", playPaper);
+        scissorsButton.removeEventListener("click", playScissors);
+    }
+}
+
 const rockButton = document.querySelector("#rock");
-rockButton.addEventListener("click", () => playRound("rock"));
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+
+//Button to play a round of the game with rock as playerSelection
+function playRock() 
+{
+    playRound("rock");
+}
+rockButton.addEventListener("click", playRock);
 
 //Button to play a round of the game with paper as playerSelection
-const paperButton = document.querySelector("#paper");
-paperButton.addEventListener("click", () => playRound("paper"));
+function playPaper() 
+{
+    playRound("paper");
+}
+paperButton.addEventListener("click", playPaper);
 
 //Button to play a round of the game with scissors as playerSelection
-const scissorsButton = document.querySelector("#scissors");
-scissorsButton.addEventListener("click", () => playRound("scissors"));
+function playScissors() 
+{
+    playRound("scissors");
+}
+scissorsButton.addEventListener("click", playScissors);
+
+const computerSText = document.querySelector(".computerS");
 
 //Displays the current scores of the player and the computer
 const results = document.querySelector("#results");
